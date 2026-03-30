1d19187be6dc2d22cf2ccb7e35188b8331758ef6
ismaelTelmedia <ircasarreal@telmedia.com.mx>
2026-03-30 15:12:16 -0600

    Add CRUD operations and validation to ProjectController

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { body, param } from 'express-validator'
+import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
+
+const router = Router()
+
+router.post('/', 
+    body('projectName').notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
+    body('clientName').notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description').notEmpty().withMessage('La Decripcion del Proyecto es Obligatoria'),
+    handleInputErrors,
+    ProjectController.createProject
+)
+
+router.get('/', ProjectController.getAllProjects)
+
+router.get('/:id',
+    param('id').isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    ProjectController.getProjectById
+)
+
+router.put('/:id',
+    param('id').isMongoId().withMessage('ID no valido'),
+    body('projectName').notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
+    body('clientName').notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description').notEmpty().withMessage('La Decripcion del Proyecto es Obligatoria'),
+    handleInputErrors,
+    ProjectController.updateProject
+)
+
+router.delete('/:id',
+    param('id').isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    ProjectController.deleteProject
+)
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import expres from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
+import projectRoutes from './routes/projectRoutes'
 
 dotenv.config()
 
 connectDB()
 
 const app = expres()
+
+app.use(expres.json())
+
+// Routes
+app.use('/api/projects', projectRoutes)
+// app.use('/api/projects', )
 
 export default app
