9200eac0683b60f6a7b0607ce04e85f45d2984d1
Ismael Ramirez <ismaelramirez.dev@gmail.com>
2026-04-09 19:01:07 -0600

    add middleware for task assigment between Project and TaskController

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,12 +1,14 @@
 import type {Request, Response} from 'express'
+import Task from '../models/Task'
 
 export class TaskController {
     static createTask = async (req: Request, res: Response) => {
-        
-        const { projectId } = req.params
-        console.log(projectId)
         try {
-
+            const task = new Task(req.body)
+            task.project = req.project._id
+            req.project.tasks.push(task._id)
+            await Promise.allSettled([task.save(), req.project.save()])
+            res.send('Tarea creada correctamente')
         }
         catch( error) {
             console.log(error)
--- src/middleware/project.ts
+++ src/middleware/project.ts
@@ -1,10 +1,27 @@
 import type {Request, Response, NextFunction} from 'express'
+import Project, { IProject } from '../models/Projects'
+
+// reescritura del scop global del request para obtener los datos de la url
+declare global {
+    namespace Express {
+        interface Request {
+            project: IProject
+        }
+    }
+}
 
 export async function validateProjectExist(req:Request, res: Response, next: NextFunction) {
     try {
-
+        const { projectId } = req.params
+        const project = await Project.findById(projectId)
+        if(!project) {
+            const error = new Error('Proyecto no encontrado')
+            return res.status(404).json({error: error.message})
+        }
+        req.project = project
+        next()
     } catch (error) {
-        
+        res.status(500).json({error: 'Hubo un error'})
     }
     
 }
