6be232afc5c011568e23a26aeade55cdcfaadfa9
Ismael Ramirez <ismaelramirez.dev@gmail.com>
2026-06-16 14:36:41 -0600

    complete Authentication with JWT

---
+++ src/middleware/auth.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction} from 'express'
+import jwt, { decode } from 'jsonwebtoken'
+import User, { IUser } from '../models/User'
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser
+        }
+    }
+}
+
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+    console.log()
+    const bearer = req.headers.authorization
+    if(!bearer) {
+        const error = new Error('Unauthorized')
+        return res.status(401).json({error: error.message})
+    }
+
+    const [, token] = bearer.split(' ')
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+        if(typeof decoded === 'object' && decoded.id) {
+            const user = await User.findById(decoded.id).select('_id name email')
+            if(user) {
+                req.user = user
+            } else {
+                res.status(500).json({error: 'Invelid Token'})
+            }
+        }
+    } catch (error) {
+        res.status(500).json({error: 'Invelid Token'})
+    }
+
+    next()
+
+}
